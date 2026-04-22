5e07bc0efd1f5ff154bff0fcbfd32af65ee77fc8
emilstrandheim56-dotcom <emilstrandheim56@gmail.com>
2026-04-23 01:26:43 +0700

    Add files via upload

---
+++ Experiences.jsx
@@ -0,0 +1,13 @@
+import WorkshopCard from "../components/WorkshopCard";
+
+export default function Experiences() {
+  return (
+    <div>
+      <h1>Book an Experience</h1>
+
+      <WorkshopCard title="Baking Class" />
+      <WorkshopCard title="Art Therapy" />
+      <WorkshopCard title="Scent Lab" />
+    </div>
+  );
+}
